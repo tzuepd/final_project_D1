53e6ce395285c17be1bdd30bb5e85ca0db83ffed
Tani <tze203@nyu.edu>
2025-11-17 13:47:11 -0500

    volume mock

--- final_project_D1.ino
+++ final_project_D1.ino
@@ -18,24 +18,34 @@ AudioControlSGTL5000 sgtl5000_1;  //xy=461,458
 // GUItool: end automatically generated code
 
 int filterPod = A13;
+int volumePod = A12;
 
 void setup() {
   AudioMemory(12);
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.5);
   sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
-  amp1.gain(0.2);
+  sgtl5000_1.micGain(55);
+  amp1.gain(0.15);
   Serial.begin(9600);
 }
 
 void loop() {
+  lowPassFilter();
+  //volumeManager();
+}
 
+void lowPassFilter() {
   int filterControl = analogRead(filterPod);
-
-  int cutoff = map(filterControl, 0, 1023, 150, 5500);
-
+  int cutoff = map(filterControl, 0, 1023, 150, 3000);
   filter1.frequency(cutoff);
-
   delay(5); 
 }
+
+//void volumeManager() {
+  //int volume = analogRead(volumePod);
+  //int level = map(volume, 0, 1023, 0, 800) / 1000.0;
+ //sgtl5000_1.dacVolume(volumePod);
+ //delay(5);
+//}
 
